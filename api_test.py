d23c63205e2621a0a462ffaa770f3600d1c0ddde
DrHagha <coby4313@gmail.com>
2023-04-10 15:14:39 +0900

    api작동 확인

--- api_test.py
+++ api_test.py
@@ -1,10 +1,10 @@
 from googletrans import Translator
+
 translator = Translator()
+result = translator.translate('안녕하세요')
 
-str1 = "나는 한국인 입니다."
-str2 = "I like burger."
-result1 = translator.translate(str1, dest='en')
-result2 = translator.translate(str2, dest='ko')
-
-print(f"나는 한국인 입니다. => {result1.text}")
-print(f"I like burger. => {result2.text}")
+print(result.src)
+print(result.dest)
+print(result.origin)
+print(result.text)
+print(result.pronunciation)
