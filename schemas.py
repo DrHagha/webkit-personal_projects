b0a88a6fd98d7655d0a76054ebde419a078613da
DrHagha <coby4313@gmail.com>
2023-04-19 17:32:36 +0900

    테스트 성공

--- schemas.py
+++ schemas.py
@@ -1,30 +1,37 @@
 from typing import Union
+
 from pydantic import BaseModel
 
+
 class ItemBase(BaseModel):
-    title : str
-    description : Union[str, None] = None
-    
-class ItemsCreate(ItemBase):
+    title: str
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
     pass
 
-class Item(ItemsCreate):
-    id : int
-    owner_id : int
-    
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
     class Config:
         orm_mode = True
-        
+
+
 class UserBase(BaseModel):
-    email : str
-    
+    email: str
+
+
 class UserCreate(UserBase):
-    password : str
-    
+    password: str
+
+
 class User(UserBase):
-    id : int
-    id_active : bool
-    items : list[Item] = []
-    
+    id: int
+    is_active: bool
+    items: list[Item] = []
+
     class Config:
         orm_mode = True
